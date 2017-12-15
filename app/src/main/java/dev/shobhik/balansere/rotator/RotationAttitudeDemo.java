c85689c7f5bd42bcaf5e2c2710466e67d1f50098
Shobhik Ghosh <shobhik.ghosh@gmail.com>
2017-12-14 22:22:50 -0800

    Improve UI

--- app/src/main/java/dev/shobhik/balansere/rotator/RotationAttitudeDemo.java
+++ app/src/main/java/dev/shobhik/balansere/rotator/RotationAttitudeDemo.java
@@ -1,9 +1,16 @@
 package dev.shobhik.balansere.rotator;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import dev.shobhik.balansere.R;
@@ -19,10 +26,31 @@ public class RotationAttitudeDemo extends AppCompatActivity implements Orientati
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Context mContext = this;
     setContentView(R.layout.main);
+
+    Configuration configuration = mContext.getResources().getConfiguration();
+    int screenWidthDp = configuration.screenWidthDp;
+//    int widthdiff = screenWidthDp % 100;
+//    int idealFrameSize = screenWidthDp - widthdiff;
+    DisplayMetrics displayMetrics = mContext.getResources().getDisplayMetrics();
+    float pxHeight = displayMetrics.heightPixels;
+    int pxWidth = displayMetrics.widthPixels;
+    int widthdiff = pxWidth % 100;
+    int idealFrameSize = pxWidth - widthdiff;
 
     mOrientation = new Orientation(this);
     mAttitudeIndicator = (AttitudeIndicator) findViewById(R.id.attitude_indicator);
+    ViewGroup.LayoutParams params = mAttitudeIndicator.getLayoutParams();
+    params.width = idealFrameSize;
+    params.height = idealFrameSize;
+//    FrameLayout.LayoutParams params1 = new FrameLayout.LayoutParams(params);
+    FrameLayout.LayoutParams params1 = new FrameLayout.LayoutParams(idealFrameSize, idealFrameSize, Gravity.CENTER_HORIZONTAL);
+    mAttitudeIndicator.setLayoutParams(params1);
+    ViewGroup.LayoutParams params2 = mAttitudeIndicator.getLayoutParams();
+
+    Log.v("Rotator", "New size: " + idealFrameSize + " | " + params.width +  " | " + params2.width + " | " + mAttitudeIndicator.getLayoutParams().width + " | " + mAttitudeIndicator.getWidth());
+
 
     tvX = findViewById(R.id.tv_x);
     tvY = findViewById(R.id.tv_y);
